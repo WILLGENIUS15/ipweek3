8f6bc3cdbeb85c2932d0eaaa57267f9e48280402
fidel omondi <willgenius@gmail.com>
2019-02-25 17:12:33 +0300

    radio namefixed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
-  $(document).ready(function () {
-     $("#form1").submit(function (event){
-        event.preventDefault();
-         var val1 = parseInt($("[name = variable]:checked").val());
-         var val2 = parseInt($("[name = sub]:checked").val());
-         var val3 = parseInt($("[name = con]:checked").val());
-  	     var val4 = parseInt($("input[name = t]:checked").val());
-         var val5 = parseInt($("input[name = b]:checked").val());
-         var val6 = parseInt($("input[name = c]:checked").val());
-         var val7 = parseInt($("input[name = d]:checked").val());
-         var val8 = parseInt($("input[name = e]:checked").val());
-         var val9 = parseInt($("input[name = f]:checked").val());
-         var val10 = parseInt($("input[name = g]:checked").val());
+$(document).ready(function () {
+   $("#form1").submit(function (event){
+      event.preventDefault();
+       var val1 = parseInt($("[name=variable]:checked").val());
+       var val2 = parseInt($("[name=sub]:checked").val());
+       var val3 = parseInt($("[name=con]:checked").val());
+       var val4 = parseInt($("input[name=t]:checked").val());
+       var val5 = parseInt($("input[name=b]:checked").val());
+       var val6 = parseInt($("input[name=c]:checked").val());
+       var val7 = parseInt($("input[name=d]:checked").val());
+       var val8 = parseInt($("input[name=e]:checked").val());
+       var val9 = parseInt($("input[name=f]:checked").val());
+       var val10 = parseInt($("input[name=g]:checked").val());
 
-  	     var result = parseInt(val1 + val2 + val3 + val4 + val5 + val6 +val7 + val8 + val9 + val10);
+       var result = parseInt(val1 + val2 + val3 + val4 + val5 + val6 +val7 + val8 + val9 + val10);
 
-         if (result > 80) {
-         $("#output").text("Your score is: " + result);
-         }
-          else if (result >= 50 && result <= 80) {
-         $("#output").text("Your score is: " + result);
-         }
-         else { (result < 50)
-         $("#output").text("Your score is: " + result);
-       }
-  });
-  });
+       $("#output").text("Your score is: " + result);
+});
+});
